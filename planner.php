4e0f18b278f7a648c3de2f30d6d71ee00c50c432
Robel Beyene <robelrb@gmail.com>
2018-11-26 18:14:29 -0500

    php for login
    Some php for the login page. Made changes to index.html, planner.php. Current placeholder account is "username: username, password: password".

--- planner.php
+++ planner.php
@@ -1,3 +1,41 @@
+<?php
+
+	if (isset($_POST["login"])) {
+		session_start();
+		$username = trim($_POST["username"]);
+	    $password = trim($_POST["password"]);
+		$host="localhost";
+		$user="dbuser";
+		$serverpassword="";
+		$database="scheduleusers";
+		$db_connection=new mysqli($host, $user, $serverpassword, $database);
+		if ($db_connection->connect_error) {
+			echo "<br>database is not set up properly/was not able to properly connect to dB. This page is invalid<br>";
+			die($db_connection->connect_error);
+		}
+
+		$values = $db_connection->query("select * from users where username='$username' and password='$password'");
+		if($values->num_rows > 0){
+	      $row = $values->fetch_assoc();
+		 $_SESSION['currentfirst']= $row['firstname'];
+		 $_SESSION['currentlast'] = $row['lastname'];
+	  	} else{
+		  #print("CANT FIND ENTRY");
+
+		  $db_connection->close();
+
+		  header("Location: index.html");
+	  	}
+
+
+		$db_connection->close();
+
+
+	}
+
+ ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,13 +49,13 @@
 	<div class="container">
 
 		<h1>Goals and Plans</h1>
-		<h2 id = "today"></h2> 
+		<h2 id = "today">Welcome <?php echo $_SESSION['currentfirst']; ?>, today is: </h2>
 		<h3 id = "currMonth">January 2019 Schedule</h3>
-		
+
 		<!-- These look pretty bad visually. Feel free to change them up. -->
 		<button type = "button" class = "btn btn-secondary" onclick = "updateMonth(-1)"><</button>
 		<button type = "button" class = "btn btn-secondary" onclick = "updateMonth(1)">></button>
-		
+
 		<div class="row">
 			<fieldset class="col-lg-6">
 				<legend>Goals and Tasks</legend>
@@ -34,7 +72,7 @@
 						</div>
 						<div class = "panel-body">This is a description of the task. This is a description of the task. This is a description of the task. This is a description of the task.</div>
 					</div>
-					
+
 					<div class="panel panel-default">
 						<div class = "panel-heading">
 							<div class = "row">
@@ -81,70 +119,70 @@
 		var month;
 		var year;
 		initialize();
-		
+
 		function initialize() {
 			var d = new Date();
 			var str = d.toDateString();
 			month = d.getMonth();
 			year = d.getFullYear();
-			document.getElementById("today").innerHTML = str;
+			document.getElementById("today").innerHTML +=  str;
 			updateCurrMonth();
 		}
-		
+
 		function monthStr(arg) {
 			if (arg == 0)
 				return "January";
-			
+
 			else if (arg == 1)
 				return "February";
-			
+
 			else if (arg == 2)
 				return "March";
-			
+
 			else if (arg == 3)
 				return "April";
-			
+
 			else if (arg == 4)
 				return "May";
-			
+
 			else if (arg == 5)
 				return "June";
-			
+
 			else if (arg == 6)
 				return "July";
-			
+
 			else if (arg == 7)
 				return "August";
-			
+
 			else if (arg == 8)
 				return "September";
-			
+
 			else if (arg == 9)
 				return "October";
-			
+
 			else if (arg == 10)
 				return "November";
-			
+
 			else
 				return "December";
 		}
-		
+
 		function updateMonth(i) {
 			month += i;
-			
+
 			if (month < 0) {
 				month = 11;
 				year--;
 			}
-			
+
 			else if (month > 11) {
 				month = 0;
 				year++;
 			}
-			
+
 			updateCurrMonth();
 		}
-		
+
 		function updateCurrMonth() {
 			document.getElementById("currMonth").innerHTML = monthStr(month) + " " + year + " Schedule";
 		}
