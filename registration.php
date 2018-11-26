db4af767ec7e41b695a2b82506aa416bb920435d
Kevin Hung <khung@terpmail.umd.edu>
2018-11-26 14:31:11 -0500

    almost done with user registration
    need to rearrange page so html is generated by php

--- registration.php
+++ registration.php
@@ -12,15 +12,37 @@
 <body>
 	<form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">  
 		<h1>User Registration</h1><br>
-		First Name: <input type="text" id="fname" placeholder="Kevin" required><br><br>
-		Last Name: <input type="text" id="lname" placeholder="Hung" required><br><br>
-		User Name: <input type="text" id="uname" placeholder="khung" required><br><br>
-		Password: <input type="password" id="pword" placeholder="******" required><br><br>
-		<button type="submit" class="btn btn-default" id="submit">Submit</button>
+		First Name: <input type="text" id="fname" name="fname" placeholder="Kevin" required><br><br>
+		Last Name: <input type="text" id="lname" name="lname" placeholder="Hung" required><br><br>
+		User Name: <input type="text" id="uname" name="uname" placeholder="khung" required><br><br>
+		Password: <input type="password" id="pword" name="pword" placeholder="******" required><br><br>
+		<button type="submit" class="btn btn-default" name="submit" id="submit">Submit</button>
 		<button type="reset" class="btn btn-default" id="reset">Reset</button>
 		<?php
+			#echo "here1";
 			if(isset($_POST['submit'])){
-				
+				#if ($_POST['submit'] == 'Submit') { 
+					#echo "echo2";
+					$host="localhost";
+					$user="dbuser";
+					$password="";
+					$database="scheduleusers";
+					$db_connection=new mysqli($host, $user, $password, $database); 
+					if ($db_connection->connect_error) { 
+						echo "no good";
+						die($db_connection->connect_error); 
+					}else{
+						echo "logged into dB okay.";
+					}
+				#}
+				if(isset($_POST['pword'])){
+						$query = "insert into users (firstname, lastname, username, password) values(\"".$_POST['fname']."\",\"".$_POST['lname']."\",\"".$_POST['uname']."\",\"".$_POST['pword']."\")";
+						echo $query;
+				}
+				if(isset($query)){
+					$result = $db_connection->query($query); 
+					if (!$result) { echo "<br>The user could not be created. Please check that you have a UNIQUE username under 20 characters!";}
+				}
 			}
 		?>
 	</form>
