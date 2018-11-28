4d4e3d6cbe1e44ebf29e2b7da1c52e3e2cfe516c
Kevin Hung <khung@terpmail.umd.edu>
2018-11-27 20:28:59 -0500

    no change
    added php but nothing changes because update with image in php isnt working

--- upload.php
+++ upload.php
@@ -1,5 +1,35 @@
 <?php
-echo $_POST['upload'];
+    session_start();
+	if (isset($_SESSION["username"]) || isset($_SESSION['password'])) {
+
+		if(isset($_POST["username"]) && isset($_POST["password"])){
+			$username = trim($_POST["username"]);
+			$password = trim($_POST["password"]);
+
+		} else{
+			$username = trim($_SESSION["username"]);
+			$password = trim($_SESSION["password"]);
+		}
+		$newImage=$_POST['upload'];
+		$host="localhost";
+		$user="dbuser";
+		$serverpassword="";
+		$database="scheduleusers";
+		$db_connection=new mysqli($host, $user, $serverpassword, $database);
+		if ($db_connection->connect_error) {
+			echo "<br>database is not set up properly/was not able to properly connect to dB. This page is invalid<br>";
+			die($db_connection->connect_error);
+		}else{
+		#echo $_POST['upload'];
+		#echo $_SESSION['username'];
+		#echo $_SESSION['password'];
+			$values = $db_connection->query("update users set picture='$newImage' 'where username='$username' and password='$password'");	
+			$db_connection->close();
+		}
+
+	}else{
+		echo "hi";
+	}
 
 
 
